9cce7aacc2b746027d6a76b3407dcb116168d464
qypol342 <qypol342@gmail.com>
2024-02-19 23:35:03 +0100

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # letterbox-badge
-Show of poor taste in your readme
+Show your poor cinematic tastes in your README
+
+## Usage
+simple:
+`![my poor taste](https://letterboxd-badge.vercel.app/<letterboxd-user-name>)`
+
+## Example
+![my poor taste](https://letterboxd-badge.vercel.app/qypol)
--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const port = 3000;
 
 const svgFilePath = '/source.svg';
 
-const initialSvgContent = `<svg width="360" height="180" viewBox="0 0 360 180" fill="none" xmlns="http://www.w3.org/2000/svg"
+const initialSvgContent = `<svg width="380" height="180" viewBox="0 0 360 180" fill="none" xmlns="http://www.w3.org/2000/svg"
 xmlns:xlink="http://www.w3.org/1999/xlink">
-<rect width="360" height="180" rx="30" fill="url(#pattern0)" />
+<rect width="380" height="180" rx="30" fill="url(#pattern0)" />
 <g filter="url(#filter0_d_1_19)">
     <rect x="20" y="37" width="70" height="105" fill="url(#pattern1)"
         shape-rendering="crispEdges" />
